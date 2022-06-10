065494e1307ba93af8b834c8da8fa55dc9683add
Sidemar <sidemarschi@outlook.com>
2022-06-10 12:17:29 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato( );
+		Livros livros = new Livros ();
+		
+		
+		
+		System.out.println( gato );
+		System.out.println(livros);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hellow Word" +" " +  (a + b) );*/
+          
+	}
+
+}
+
+class Livros {
+	
+	
+	
+}
+
