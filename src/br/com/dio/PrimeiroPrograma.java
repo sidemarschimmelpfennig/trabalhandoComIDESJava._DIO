3b6969fd5547f2d0c0a3cae0cfbfc73390caf371
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2022-07-05 15:39:57 -0300

    commit Inicial para estudos em java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 14;
+        System.out.println("hello word vale = " + (a + b));
+
+    }
+}
+
